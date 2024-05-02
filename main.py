a4d887e2bac185041729a89411fadc5bd2eba6d2
Devon Browning <browningdevon@gmail.com>
2024-05-01 21:00:56 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Echo String
+my_input = input("Enter some text here!>> ")
+print(my_input)
+
+# Exercise 2: Adding a number to an integer
+num1 = input("Type a number here: ")
+print(int(num1) + int(1))
+
+# Exercise 3: Adding a number to a float
+my_float = input("Enter another number: ")
+print(float(my_float) + float(0.5))
+
+# Exercise 4: Adding Two Floats
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print("The sum is " + str(result))
+
+# Exercise 5: Multiplying Floats
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) * float(num2)
+print("The product is " + str(result))
+
+# Exercise 6: Dividing Integers
+num1 = input("Enter a number: ")
+num2 = input("Enter one last number: ")
+result = int(num1) / int(num2)
+print("The result is " + str(result))
